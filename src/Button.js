8201578e3e37de80643d257ae07963e691aa87a3
Paul Morar <paul.morar.9612@gmail.com>
2023-05-22 20:41:07 +0300

    fixed text-color bug

--- src/Button.js
+++ src/Button.js
@@ -16,20 +16,24 @@ function Button({
 
 // Sets styling for each button type
 
-    const classes = className(rest.className, 'flex items-center px-3 py-1.5 border', {
-        'border-blue-600 bg-blue-500 text-white': primary,
-        'border-gray-900 bg-gray-800 text-white': secondary,
-        'border-green-600 bg-green-500 text-white': success,
-        'border-yellow-600 bg-yellow-500 text-white': warning,
-        'border-red-600 bg-red-500 text-white': danger,
-        'rounded-full': rounded,
-        'bg-white': outline,
-        'text-blue-600': outline && primary,
-        'text-gray-900': outline && secondary,
-        'text-green-600': outline && success,
-        'text-yellow-600': outline && warning,
-        'text-red-600': outline && danger,
-    });
+const classes = className(
+    rest.className,
+    'flex items-center px-3 py-1.5 border',
+    {
+      'border-blue-500 bg-blue-500 text-white': primary,
+      'border-gray-900 bg-gray-900 text-white': secondary,
+      'border-green-500 bg-green-500 text-white': success,
+      'border-yellow-400 bg-yellow-400 text-white': warning,
+      'border-red-500 bg-red-500 text-white': danger,
+      'rounded-full': rounded,
+      'bg-white': outline,
+      'text-blue-500': outline && primary,
+      'text-gray-900': outline && secondary,
+      'text-green-500': outline && success,
+      'text-yellow-400': outline && warning,
+      'text-red-500': outline && danger,
+    }
+    );
 
     return <button {...rest} className={classes}>{children}</button>
 }
